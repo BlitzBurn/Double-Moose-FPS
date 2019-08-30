5d03b8b58bc0d11551f1702bf4baad414e5a074d
Jonatan Karlsson <JK98@Live.se>
2019-08-30 09:26:00 +0200

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float horizontalSpeed;
+    public float verticalSpeed;
+
+    private float rotHor=0.0f;
+    private float rotVer=0.0f;
+
+    void Start()
+    {
+        
+    }
+
+   
+    void FixedUpdate()
+    {
+        rotHor += horizontalSpeed * Input.GetAxis("Mouse X");
+        rotVer -= verticalSpeed * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(rotVer, rotHor, 0.0f);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Double-Moose-FPS
+ 
